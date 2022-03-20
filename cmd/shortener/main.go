56dba1fa8eb10889f8b020ee0a10f040ea37315e
Галкин Сергей <galkinsa@edu.mos.ru>
2022-03-21 02:09:39 +0300

    feat: added chi router

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/go-chi/chi/v5"
 	"github.com/sergalkin/go-url-shortener.git/internal/app/shortener"
 	"github.com/sergalkin/go-url-shortener.git/internal/app/storage"
 	"github.com/sergalkin/go-url-shortener.git/internal/app/utils"
@@ -13,15 +14,18 @@ import (
 func main() {
 	rand.Seed(time.Now().UnixNano())
 
+	r := chi.NewRouter()
+
 	memoryStorage := storage.NewMemory()
 	sequence := utils.NewSequence()
 	service := shortener.NewURLShortenerService(memoryStorage, sequence)
 	handler := shortener.NewURLShortenerHandler(service)
 
-	http.HandleFunc("/", handler.URLHandler)
+	r.Get("/", handler.URLHandler)
 
 	server := &http.Server{
-		Addr: ":8080",
+		Addr:    ":8080",
+		Handler: r,
 	}
 
 	log.Fatalln(server.ListenAndServe())
